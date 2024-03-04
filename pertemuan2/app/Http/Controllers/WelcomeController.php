4517257e55c994ffc24d23b5c823f2d888d66657
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-04 21:27:36 +0700

    update praktikum JS 2

---
+++ pertemuan2/app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use COM;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function hello(){
+        return 'hello World';
+    }
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name','Andi')
+            ->with('occupation','Astronaut');
+    }
+}

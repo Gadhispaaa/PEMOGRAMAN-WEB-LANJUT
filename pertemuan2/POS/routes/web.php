b3c9cb4e0ee1e877e20fe4e410000dbe76a8ba51
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-04 21:51:41 +0700

    tugas POS

--- pertemuan2/POS/routes/web.php
+++ pertemuan2/POS/routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/penjualan', [PenjualanController::class, 'showPOS'])->name('penjualan.pos');
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('category')->group(function () {
+    Route::get('food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('home-care', [ProductController::class, 'homeCare']);
+    Route::get('baby-kid', [ProductController::class, 'babyKid']);
 });
+
+
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'showProfile'])->name('profile');

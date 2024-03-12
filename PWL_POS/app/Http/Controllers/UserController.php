12cabddc891b530d10c7fa451a831d5c8767cada
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-12 23:19:22 +0700

    update js 4

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -9,10 +9,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $data = [
-            'nama'=>'Pelanggan Pertama',
+        /*$data = [
+            'level_id'=> 2,
+            'username'=> 'manager 2',
+            'nama'=> 'Manager 2',
+            'password'=> Hash::make('12345')
         ];
-        UserModel :: where('username', 'customer-1')->update($data);
+        UserModel::create($data);*/
+
+        /*$data = [
+            'level_id'=> 3,
+            'username'=> 'manager 3',
+            'nama'=> 'Manager 3',
+            'password'=> Hash::make('12345')
+        ];
+        UserModel::create($data);*/
+
 
 
         // dd(UserModel :: where('username', 'customer-1')->get());
@@ -32,7 +44,12 @@ class UserController extends Controller
 
 
 
-        $user = UserModel::all();
+        //$user = UserModel::all();
+        //$user = UserModel::find(1);
+        //$user = UserModel::where('level_id', 1)->first();
+        //$user = UserModel::firstWhere('level_id', 1);
+        //$user = UserModel::findOr(1, ['username', 'nama'], function (){abort(404);});
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){abort(404);});
         return view('user', ['data' => $user]);
         }
 }

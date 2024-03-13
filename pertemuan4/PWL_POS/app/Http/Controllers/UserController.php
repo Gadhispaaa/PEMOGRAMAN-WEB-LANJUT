aeae648b02f4c2d9065cdd67a2875137c51a3326
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-13 21:47:51 +0700

    update

--- pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
+++ pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
@@ -72,15 +72,61 @@ class UserController extends Controller
     //         'username' => 'manager',
     //         'nama' => 'Manager'
     //     ]);
-       $user = UserModel::firstOrNew(
-        [
-            'username' => 'manager33',
-            'nama' => 'Manager Tiga Tiga',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-         ]);
-         $user->save();
-       return view('user', ['data' => $user]);
+    //    $user = UserModel::firstOrNew(
+    //     [
+    //         'username' => 'manager33',
+    //         'nama' => 'Manager Tiga Tiga',
+    //         'password' => Hash::make('12345'),
+    //         'level_id' => 2
+    //      ]);
+    //      $user->save();
+    //    return view('user', ['data' => $user]);
         
+    //=======================================================
+    //  $user = UserModel::create([
+    //         'level_id' => 2,
+    //         'username' => 'manager5',
+    //         'nama' => 'Manager Lima Lima',
+    //         'password' => Hash::make('12345')
+    //     ]);
+
+    //     $user->username = 'manager56';
+
+    //     $user->isDirty();
+    //     $user->isDirty('username');
+    //     $user->isDirty('nama');
+    //     $user->isDirty(['nama', 'username']);
+
+    //     $user->isClean();
+    //     $user->isClean('username');
+    //     $user->isClean('nama');
+    //     $user->isClean(['nama', 'username']);
+
+    //     $user->save();
+
+    //     $user->isDirty();
+    //     $user->isClean();
+
+    //     dd($user->isDirty());
+    //====================================================
+    //  $user = UserModel::create([
+    //         'level_id' => 2,
+    //         'username' => 'manager11',
+    //         'nama' => 'Manager11',
+    //         'password' => Hash::make('12345')
+    //     ]);
+
+    //     $user->username = 'manager12';
+
+    //     $user->save();
+
+    //     $user->wasChanged();
+    //     $user->wasChanged('username');
+    //     $user->wasChanged(['username', 'level_id']);
+    //     $user->wasChanged('nama');
+    //     dd($user->wasChanged(['nama', 'username']));
+    //=========================================================
+    $user = UserModel::all();
+    return view ('user', ['data' => $user]);
         }
 }

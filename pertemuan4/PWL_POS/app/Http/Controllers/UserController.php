a74c77adef46dfcc4aebafa5f5b51b2a7be18690
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-13 23:31:49 +0700

    JOBSHEET 4

--- pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
+++ pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
@@ -126,20 +126,62 @@ class UserController extends Controller
     //     $user->wasChanged('nama');
     //     dd($user->wasChanged(['nama', 'username']));
     //=========================================================
-    $user = UserModel::all();
-    return view ('user', ['data' => $user]);
-        }
-    
-    public function tambah() {
+    // $user = UserModel::all();
+
+    $user = userModel::with('level')->get();
+
+        return view('user', ['data' => $user]);
+
+    }
+
+    public function tambah()
+    {
         return view('user_tambah');
     }
-    public function tambah_simpan(Request $request) {
+
+    public function tambah_simpan(Request $request)
+    {
         UserModel::create([
             'username' => $request->username,
             'nama' => $request->nama,
-            'password' => Hash::make('$request->password'), // ??
+            'password' => Hash::make($request->password),
             'level_id' => $request->level_id
         ]);
+
         return redirect('/user');
     }
+
+    public function ubah($id)
+    {
+        $user = userModel::find($id);
+
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = userModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make($request->password);
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+
+    }
+
+    public function hapus($id)
+    {
+        $user = userModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
+
+    }
+
+
+
 }

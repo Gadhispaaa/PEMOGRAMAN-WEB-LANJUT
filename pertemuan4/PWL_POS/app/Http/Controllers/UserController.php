b01601da4459125c38ba8aa7e230273966bbd16f
Gadhispaaa <gdspramestya7@gmail.com>
2024-03-13 22:37:00 +0700

    update

--- pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
+++ pertemuan4/PWL_POS/app/Http/Controllers/UserController.php
@@ -129,4 +129,17 @@ class UserController extends Controller
     $user = UserModel::all();
     return view ('user', ['data' => $user]);
         }
+    
+    public function tambah() {
+        return view('user_tambah');
+    }
+    public function tambah_simpan(Request $request) {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'), // ??
+            'level_id' => $request->level_id
+        ]);
+        return redirect('/user');
+    }
 }

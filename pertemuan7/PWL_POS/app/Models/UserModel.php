3c38ebaa3957fbfcf5ea1d7840c434e052874f82
Gadhispaaa <gdspramestya7@gmail.com>
2024-04-02 00:05:33 +0700

    update

--- pertemuan7/PWL_POS/app/Models/UserModel.php
+++ pertemuan7/PWL_POS/app/Models/UserModel.php
@@ -6,15 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+class userModel extends Model
 {
     use HasFactory;
 
     protected $table = 'm_user';
+    public $timestamps = true;
     protected $primaryKey = 'user_id';
 
-    protected $fillable = ['level_id','username','nama', 'password'];
-    
+    protected $fillable =  [
+        'user_id', 
+        'level_id', 
+        'username', 
+        'nama', 
+        'password'
+    ];
+
+
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
